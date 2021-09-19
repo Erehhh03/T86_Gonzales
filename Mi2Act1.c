086664edb57e94491a6668523a9649714c4a5ca8
Lord Edmund Gonzales <89230603+Erehhh03@users.noreply.github.com>
2021-09-19 16:05:26 +0800

    Add files via upload

---
+++ Mi2Act1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char charaterName[] = "Lord Edmund Gonzales";
+    int age = 19;
+    printf("My name is %s, \n", charaterName);
+    printf("And I am %d years old.\n", age);
+    return 0;
+}
